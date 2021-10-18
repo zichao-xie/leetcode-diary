9ebbc8d18416f9577040d05f68058dbf498e72de
xzc <1335718228@qq.com>
2021-10-18 19:43:23 +0800

    addTwoNumbers

--- src/ListNode.java
+++ src/ListNode.java
@@ -1,7 +1,13 @@
-package PACKAGE_NAME;/**
- * @author  xzc
- * @time  2021/10/18 19:05
+/**
+ * @author xzc
  * @version 1
- * @number 
- */public class ListNode {
+ * @time 2021/10/18 19:05
+ * @number
+ */
+public class ListNode {
+    int val;
+    ListNode next;
+    ListNode(){}
+    ListNode(int val) {this.val = val;}
+    ListNode(int val ,ListNode next) {this.val = val;this.next = next;}
 }
--- src/addTwoNumbers.java
+++ src/addTwoNumbers.java
@@ -1,7 +1,32 @@
-package PACKAGE_NAME;/**
- * @author  xzc
- * @time  2021/10/18 19:04
+/**
+ * @author xzc
  * @version 1
- * @number 
- */public class addTwoNumbers {
+ * @time 2021/10/18 19:04
+ * @number 2
+ */
+public class addTwoNumbers {
+    /**
+     * @author  xzc
+     * @time  2021/10/18 19:42
+     * @description 没啥
+     */
+    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+        ListNode pre  = new ListNode();
+        ListNode cur = pre;
+        int carry = 0;
+
+        while (l1!=null||l2!=null){
+            cur.next = new ListNode();
+            cur = cur.next;
+            int x= l1==null?0:l1.val;
+            int y= l2==null?0:l2.val;
+            cur.val = (x+y+carry)%10;
+            carry=(x+y+carry)/10;
+
+            if (l1!=null) l1 = l1.next;
+            if (l2!=null) l2 = l2.next;
+        }
+        if (carry!=0) cur.next = new ListNode(carry);
+        return pre.next;
+    }
 }
